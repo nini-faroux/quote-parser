f0f730b07d22cf3ad441e2b9a35691fa0bf1eed3
nini-faroux <nfarrel1@tcd.ie>
2020-02-03 16:28:19 +0000

    add pcap header types

--- src/Types.hs
+++ src/Types.hs
@@ -1,6 +1,27 @@
 module Types where
 
-import           Data.Time (TimeOfDay)
+import Data.Time (TimeOfDay)
+import Data.Word
+import Data.Int
+
+data GlobalHeader = 
+  GlobalHeader { 
+    magicNumber   :: Word32 
+  , versionNumber :: Word16
+  , versionMinor  :: Word16 
+  , thisZone      :: Int32 
+  , sigfigs       :: Word32 
+  , snaplen       :: Word32 
+  , network       :: Word32 
+  } deriving (Show) 
+
+data PacketHeader = 
+  PacketHeader { 
+    tsSec   :: Word32 
+  , tsUsec  :: Word32 
+  , inclLen :: Word32 
+  , origLen :: Word32 
+  } deriving (Show)
 
 data QuoteMessage = 
   QuoteMessage { 
