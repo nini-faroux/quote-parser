d738c4492be7b3f4cc2fbfe0771529eaab110a40
nini-faroux <nfarrel1@tcd.ie>
2020-02-10 13:50:10 +0000

    use optparse-applicative to parse user input

--- app/Main.hs
+++ app/Main.hs
@@ -2,31 +2,26 @@
 
 module Main where
 
+import Options.Applicative
+
 import Stream (stream)
 import StreamSorted (streamSorted)
-import System.Environment (getArgs)
 
-main :: IO ()
-main = do 
-  args <- getArgs' 
-  let file = head args
-  if length args == 1 
-  then stream file
-  else streamSorted file
+main :: IO () 
+main = execParser opts >>= run 
+  where 
+    parser = Quote <$> argument str (metavar "<FILEPATH>") 
+                   <*> switch (short 'r' <> long "sort" <> help "Sort output on packet accept time") 
+    opts = info parser mempty 
 
-getArgs' :: IO [String]
-getArgs' = getArgs >>= parse
-  where
-    parse :: [String] -> IO [String]
-    parse [] = return [defaultFile]
-    parse [filePath] = return [filePath]
-    parse [filePath, sortFlag] 
-      | sortFlag /= "r" = error usage
-      | otherwise       = return [filePath, sortFlag] 
-    parse _ = error usage
+data Quote = 
+  Quote { 
+    inputFile :: FilePath
+  , sortedOutput :: Bool 
+  } 
 
-usage :: String
-usage = "Usage: stock-parser <filepath> <optional: -r>"
-
-defaultFile :: String 
-defaultFile = "./data/mdf-kospi200.20110216-0.pcap"
+run :: Quote -> IO () 
+run opts 
+  | sortedOutput opts = streamSorted fp 
+  | otherwise         = stream fp 
+  where fp = inputFile opts 
