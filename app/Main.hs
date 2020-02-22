626c37f2c0c43ef4f5cef32e40a37880b478e353
nini-faroux <nfarrel1@tcd.ie>
2020-02-22 02:38:40 +0000

    define stream in terms of streamSorted, avoid duplication

--- app/Main.hs
+++ app/Main.hs
@@ -3,32 +3,33 @@ module Main where
 import Options.Applicative (execParser, str, metavar, switch, short, long, help, argument, info)
 import qualified Data.ByteString as BS
 import Stream (stream)
-import StreamSorted (streamSorted)
 
 -- | Program entry point, requires path to input file
 -- and optional flag for sorting output
 main :: IO () 
-main = execParser opts >>= run 
+main = execParser options >>= run 
   where 
     parser = Quote <$> argument str (metavar "<FILEPATH>") 
                    <*> switch (short 'r' <> long "sort" <> help "Sort output on packet accept time") 
-    opts = info parser mempty 
+    options = info parser mempty 
 
 -- | Record representing command inputs
 data Quote = 
   Quote { 
-    inputFile :: FilePath
-  , sortedOutput :: Bool 
+    inputFilePath :: FilePath
+  , sortedOutput  :: Bool 
   } 
 
 -- | If the user supplies '-r' or '--sort' flag 
--- apply the 'streamSorted' function to the input
--- otherwise apply stream function
+-- sort the output otherwise output in given order
 run :: Quote -> IO () 
-run opts 
-  | sortedOutput opts = skipGlobalHeader fp >> streamSorted source 
-  | otherwise         = skipGlobalHeader fp >> stream source 
-  where fp = inputFile opts 
+run options 
+  | sortedFlag = stream' True
+  | otherwise  = stream' False
+  where 
+    stream' sortFlag = skipGlobalHeader filePath >> stream sortFlag source
+    sortedFlag = sortedOutput options
+    filePath = inputFilePath options 
 
 -- | The first 24 bytes are just the pcap global header
 skipGlobalHeader :: FilePath -> IO ()
--- src/Stream.hs
+++ src/Stream.hs
@@ -1,25 +1,70 @@
 module Stream where 
 
-import Types (QuotePacket)
+import Types (QuotePacket(..))
 import Parser (packetParser)
 import Conduit (ConduitT, ResourceT, runConduitRes, sourceFile, await, (.|))
 import Data.Conduit.Attoparsec (ParseError, conduitParserEither)
+import Data.Time.LocalTime (todSec)
+import Data.Foldable (traverse_)
+import Data.Fixed (Pico, mod')
 import Control.Monad.IO.Class (liftIO)
 
--- | See StreamSorted comments, the same idea except here we don't need to sort the data 
--- based on the quote accept time, simply print packets in the order they are passed on
-stream :: FilePath -> IO ()
-stream fp = runConduitRes $ sourceFile fp .| conduitParserEither packetParser .| parserSink
+-- | Conduit pipeline, parserSink awaits data from upstream
+-- upstream source file pipes data downstream to the packet parser which attempts to parse each chunk
+-- if successful it sends the parsed data to parserSink which processes it and then awaits again
+stream :: Bool -> FilePath -> IO ()
+stream sortFlag fp = runConduitRes $ sourceFile fp .| conduitParserEither packetParser .| parserSink sortFlag [] 3
 
-parserSink :: ConduitT (Either ParseError (a, Either () QuotePacket)) o (ResourceT IO) ()
-parserSink = do 
-  nextMessage <- await 
-  case nextMessage of 
-    Nothing -> liftIO $ putStrLn "End of Input"
+-- | Sink for printing quote packets 
+-- Use a temporary list buffer to store quotes within 3 seconds intervals 
+-- (assumption from problem statement that difference between packetTime and quote accept time is never more than 3 seconds)
+-- When we reach the end of the next 3 second interval, if the sort flag is set we sort and print the quotes, otherwise 
+-- print the buffer as is, and then reset the buffer to empty list and update the limit to 3 seconds ahead
+parserSink :: Bool -> [QuotePacket] -> Pico -> ConduitT (Either ParseError (a, Either () QuotePacket)) o (ResourceT IO) ()
+parserSink sortFlag buffer limit = do 
+  nextPacket <- await 
+  case nextPacket of 
+    Nothing -> liftIO (printQuotes sortFlag buffer) >> endOfInput
     Just parsed -> 
       case parsed of 
-      Left err -> liftIO $ print err 
-      Right (_, quoteMessage) -> 
-        case quoteMessage of 
-          Left () -> parserSink 
-          Right quote -> liftIO (print quote) >> parserSink 
+        Left err -> printError err
+        Right (_, packet) -> 
+          case packet of 
+            Left () -> loop buffer 
+            Right quotePacket -> 
+              if checkLimit limit quotePacket 
+              then liftIO (printQuotes sortFlag (appendQuote quotePacket)) >> resetLoop
+              else loop (appendQuote quotePacket) 
+  where 
+    resetLoop = parserSink sortFlag [] (updateLimit limit) 
+    loop quotes = parserSink sortFlag quotes limit
+    appendQuote quote = buffer ++ [quote]
+    printError e = liftIO $ print e
+    endOfInput = liftIO $ putStrLn "End of Input"
+
+-- | Sort a list of quote packets based on their acceptTime field
+sortQuotes :: [QuotePacket] -> [QuotePacket] 
+sortQuotes [] = [] 
+sortQuotes (p:qs) = sortQuotes [q | q <- qs, qt q <= pivTime] ++ [p] ++ sortQuotes [q | q <- qs, qt q > pivTime] 
+  where 
+    pivTime = acceptTime p
+    qt = acceptTime 
+
+printQuotes :: Bool -> [QuotePacket] -> IO ()
+printQuotes sortFlag quotes
+  | sortFlag  = print' (sortQuotes quotes) 
+  | otherwise = print' quotes
+  where print' = traverse_ print 
+
+-- | Check if the packet time has reached 
+-- the next 3 second limit
+checkLimit :: Pico -> QuotePacket -> Bool 
+checkLimit limit qp 
+  | packetTimeSecs == limit = True 
+  | otherwise               = False 
+  where 
+    packetTimeSecs = todSec $ packetTime qp
+
+-- | Set the next limit time 
+updateLimit :: Pico -> Pico 
+updateLimit limit = (limit + 3) `mod'` 60
